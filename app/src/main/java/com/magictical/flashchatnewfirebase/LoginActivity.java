9c15e9a9f1fa1c3499f004407d7ec067a8a7c16a
Ian KIM <magictical@gmail.com>
2018-07-11 15:04:45 +0900

    Finish LoginActivity
    Add attemptLogin() - check validation of email/password login
    showErrorDialog  - show error msg when login failed

--- app/src/main/java/com/magictical/flashchatnewfirebase/LoginActivity.java
+++ app/src/main/java/com/magictical/flashchatnewfirebase/LoginActivity.java
@@ -2,18 +2,28 @@ package com.magictical.flashchatnewfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -36,15 +46,14 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
-
         // TODO: Grab an instance of FirebaseAuth
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
         // TODO: Call attemptLogin() here
-
+        attemptLogin();
     }
 
     // Executed when Register button pressed
@@ -57,14 +66,46 @@ public class LoginActivity extends AppCompatActivity {
     // TODO: Complete the attemptLogin() method
     private void attemptLogin() {
 
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        //if(email.length() < 1 || password.length() < 5) {
+        //    Toast.makeText(this, "Please Enter Email or password", Toast.LENGTH_SHORT).show();
+        //}
+
+        if(email.length() <1 || password.length() < 5) return;
+        Toast.makeText(this, "Please enter email or password", Toast.LENGTH_SHORT).show();
 
         // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //notice for signIn process has finished
+                Log.d("FlashChat", "SignInWithMail() OnComplete: " + task.isSuccessful());
+                //if error show error msg
+                if(!task.isSuccessful()) {
+                    Log.d("FlashChat", "Problem signing in : " + task.getException());
+                    showErrorDialog("Login error!!");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
 
-
+        });
 
     }
 
     // TODO: Show error on screen with an alert dialog
+    private void showErrorDialog(String msg) {
+        new AlertDialog.Builder(this)
+                .setTitle("Login Error!")
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 
