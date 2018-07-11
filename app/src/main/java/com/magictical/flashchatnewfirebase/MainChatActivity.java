a4ea90ad60a738c484a7a60bb1549811b0146b85
Ian KIM <magictical@gmail.com>
2018-07-11 15:17:34 +0900

    Display userName on MainChat screen

--- app/src/main/java/com/magictical/flashchatnewfirebase/MainChatActivity.java
+++ app/src/main/java/com/magictical/flashchatnewfirebase/MainChatActivity.java
@@ -1,5 +1,6 @@
 package com.magictical.flashchatnewfirebase;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
@@ -21,7 +22,7 @@ public class MainChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_chat);
 
         // TODO: Set up the display name and get the Firebase reference
-
+        setupDisplayName();
 
         // Link the Views in the layout to the Java code
         mInputText = (EditText) findViewById(R.id.messageInput);
@@ -36,6 +37,13 @@ public class MainChatActivity extends AppCompatActivity {
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName() {
+        SharedPreferences preferences = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+        mDisplayName = preferences.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+        if (mDisplayName == null) {
+            mDisplayName = "Anonymous";
+        }
+    }
 
 
     private void sendMessage() {
