e350c04bb7df60c4c26b79b0a55b4a50fcd12905
Ian KIM <magictical@gmail.com>
2018-07-14 23:35:44 +0900

    Add actionListener for send message btn

--- app/src/main/java/com/magictical/flashchatnewfirebase/MainChatActivity.java
+++ app/src/main/java/com/magictical/flashchatnewfirebase/MainChatActivity.java
@@ -3,9 +3,17 @@ package com.magictical.flashchatnewfirebase;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainChatActivity extends AppCompatActivity {
@@ -15,6 +23,7 @@ public class MainChatActivity extends AppCompatActivity {
     private ListView mChatListView;
     private EditText mInputText;
     private ImageButton mSendButton;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,7 @@ public class MainChatActivity extends AppCompatActivity {
 
         // TODO: Set up the display name and get the Firebase reference
         setupDisplayName();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         // Link the Views in the layout to the Java code
         mInputText = (EditText) findViewById(R.id.messageInput);
@@ -30,9 +40,22 @@ public class MainChatActivity extends AppCompatActivity {
         mChatListView = (ListView) findViewById(R.id.chat_list_view);
 
         // TODO: Send the message when the "enter" button is pressed
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int id, KeyEvent event) {
+                sendMessage();
+                return true;
 
+            }
+        });
 
         // TODO: Add an OnClickListener to the sendButton to send a message
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
 
     }
 
@@ -49,6 +72,7 @@ public class MainChatActivity extends AppCompatActivity {
     private void sendMessage() {
 
         // TODO: Grab the text the user typed in and push the message to Firebase
+        Log.d("FlashChat", "sendMessage()has run");
 
     }
 
