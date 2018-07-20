8a01c608a4ba2b9c9c30695d55cbc8e5c94a3d7f
Ian KIM <magictical@gmail.com>
2018-07-20 18:26:01 +0900

    message box Settings for UI
    message box UI has changed
    if author is user -> right
     else -> left
    +colors, background

--- app/src/main/java/com/magictical/flashchatnewfirebase/ChatListAdapter.java
+++ app/src/main/java/com/magictical/flashchatnewfirebase/ChatListAdapter.java
@@ -2,8 +2,10 @@ package com.magictical.flashchatnewfirebase;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,16 +25,19 @@ import java.util.ArrayList;
  */
 
 public class ChatListAdapter extends BaseAdapter {
+    //생성자에 사용될 3가지 값
     private Activity mActivity;
     private DatabaseReference mDatabaseReference;
     private String mDisplayName;
+
+    //DB변경시 Data를 저장하거나 불러올 때 사용
     private ArrayList<DataSnapshot> mSnapshotList;
 
     //this will be called when DB has been changed
     private ChildEventListener mListener = new ChildEventListener() {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+            //DB 변경시
             mSnapshotList.add(dataSnapshot);
             //refresh the listView - check it if this gonna work or not
             notifyDataSetChanged();
@@ -85,7 +90,7 @@ public class ChatListAdapter extends BaseAdapter {
         return mSnapshotList.size();
     }
 
-    //change generic to InstaceMessage object
+    //change generic to InstanceMessage object
     @Override
     public InstanceMessage getItem(int position) {
         //get item by index number
@@ -103,6 +108,7 @@ public class ChatListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         //convertView : listItem
+        //setup view for the Item(InstanceMessage)
         if(convertView == null) {
             LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService((Context.LAYOUT_INFLATER_SERVICE));
             convertView = inflater.inflate(R.layout.chat_msg_row, parent, false);
@@ -116,15 +122,41 @@ public class ChatListAdapter extends BaseAdapter {
         final InstanceMessage message = getItem(position);
         final VIewHolder holder = (VIewHolder) convertView.getTag();
 
+
+        //check userInfo
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe, holder);
+
         String Author = message.getAuthor();
         holder.authorName.setText(Author);
 
         String msg = message.getMessage();
         holder.body.setText(msg);
-        Log.d("FlashChat", "getview is here");
+        Log.d("FlashChat", "getView is here");
 
         return convertView;
     }
+
+    /*
+    * Move to END, color, background if author is user else Move to right.. etc
+    * */
+    private void setChatRowAppearance(boolean isItME, VIewHolder holder) {
+        if(isItME) {
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        } else {
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+        //윗라인에서 설정한 holder의 param값을 적용함
+        //apply params settings on the holder
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
+    }
+
+
 
     //when the app leaves on the foreground
     public void cleanup() {
